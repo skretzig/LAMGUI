f13639d9e2cbdc65812970704aa51b8f29c27e16
Sebastian Kretzig <skretzig@gmail.com>
2017-08-07 13:22:59 -0500

    Initial Commit

---
+++ LAMGUI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LAMGUI
+//
+//  Created by Sebastian Kretzig on 7/8/17.
+//  Copyright © 2017 Sebastian Kretzig. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ LAMGUI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LAMGUI
+//
+//  Created by Sebastian Kretzig on 7/8/17.
+//  Copyright © 2017 Sebastian Kretzig. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
